4dfff925bd1e6e73b691d47f4c902116b20ca8b2
Daniel Belarmino <belarba@gmail.com>
2023-12-05 12:45:50 +0000

    initial commit

---
+++ lib/disbursements_api.ex
@@ -0,0 +1,18 @@
+defmodule DisbursementsApi do
+  @moduledoc """
+  Documentation for `DisbursementsApi`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DisbursementsApi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/disbursements_api_test.exs
@@ -0,0 +1,8 @@
+defmodule DisbursementsApiTest do
+  use ExUnit.Case
+  doctest DisbursementsApi
+
+  test "greets the world" do
+    assert DisbursementsApi.hello() == :world
+  end
+end
