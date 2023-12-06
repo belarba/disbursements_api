1dbdfd7c0f68178d664b080def73ae3f5f067ee8
Daniel Belarmino <belarba@gmail.com>
2023-12-06 19:11:01 +0000

    read and import merchants csv

--- lib/disbursements_api/merchants.ex
+++ lib/disbursements_api/merchants.ex
@@ -22,8 +22,32 @@ defmodule DisbursementsApi.Merchants do
     |> validate_required([:csv_id, :reference, :email, :disbursement_frequency])
   end
 
+  defp parse_csv(file_path) do
+    file_path
+    |> File.stream!()
+    |> Enum.drop(1)  # Skip the first line
+    |> CSV.decode(separator: ";", headers: false)
+    |> Enum.map(&extract_csv_row/1)
+    |> Enum.map(&handle_csv_row/1)
+  end
+
+  defp extract_csv_row({:ok, [csv_row]}), do: [csv_row]
+  defp extract_csv_row(_), do: %{}
+
+  defp handle_csv_row([csv_row]) do
+    [csv_id, reference, email, live_on, disbursement_frequency, minimum_monthly_fee] = String.split(csv_row, ";")
+    %{
+      "csv_id" => csv_id,
+      "reference" => reference,
+      "email" => email,
+      "live_on" => live_on,
+      "disbursement_frequency" => disbursement_frequency,
+      "minimum_monthly_fee" => String.to_float(minimum_monthly_fee)
+    }
+  end
+
   def insert_orders_from_csv(file_path) do
-    csv_data = DisbursementsApi.CSVParser.parse_csv(file_path)
+    csv_data = parse_csv(file_path)
 
     Enum.each(csv_data, fn order_attrs ->
       changeset = %__MODULE__{}
